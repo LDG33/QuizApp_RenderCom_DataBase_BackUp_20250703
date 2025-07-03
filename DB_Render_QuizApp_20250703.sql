d2f776c1e277fe38cb240d579ddb07fed4ed1e93
LDG33 <118735787+LDG33@users.noreply.github.com>
2025-07-03 18:28:12 +0100

    unsuccessful db backups (.sql) maybe useful QuizApp
    the reason for unsuccesful is because I did not have an access to second table "postgres" - must have messed up during set up.

---
+++ DB_Render_QuizApp_20250703.sql
@@ -0,0 +1,11 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+-- Started on 2025-07-03 18:07:10
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
---
+++ DB_Render_QuizApp_20250703_v2.sql
@@ -0,0 +1,11 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+-- Started on 2025-07-03 18:09:45
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
